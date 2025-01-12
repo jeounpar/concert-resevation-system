eb644141b2207987f8630e9aaf2edc47ed793011
jeounpar <cdex6531@gmail.com>
2025-01-12 21:37:50 +0900

    update: PointService 로그 추가

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -5,9 +5,10 @@ import { PointRepository } from './repository/point.repository';
 import { PointLogRepository } from './repository/point-log.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PointEntity, PointLogEntity } from '../entity';
+import { LogModule } from '../log/log.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PointEntity, PointLogEntity])],
+  imports: [TypeOrmModule.forFeature([PointEntity, PointLogEntity]), LogModule],
   providers: [PointService, PointRepository, PointLogRepository],
   controllers: [PointController],
   exports: [PointService],
