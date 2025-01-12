eebb6cc30a1765c6efa717ef65853b93ae2bf447
jeounpar <cdex6531@gmail.com>
2025-01-12 21:12:22 +0900

    update: e2e 테스트 초기 설정

--- test/concert.e2e-spec.ts
+++ test/concert.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Concert (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,13 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('test', async () => {
+    const response = await request(app.getHttpServer()).get('/');
+
+    expect(response.status).toBe(400);
   });
 });
