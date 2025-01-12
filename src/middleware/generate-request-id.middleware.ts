e513c7b911742a6ef9061edf2bdb4f3745b8e029
jeounpar <cdex6531@gmail.com>
2025-01-12 21:24:33 +0900

    update: 콘서트결제(concertPayment)에 로그 추가

--- src/middleware/generate-request-id.middleware.ts
+++ src/middleware/generate-request-id.middleware.ts
@@ -2,13 +2,11 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { AsyncLocalStorageService } from '../log/async-local-storage.service';
-import { MyCustomLogger } from '../log/my-custom-logger';
 
 @Injectable()
 export class GenerateRequestIdMiddleware implements NestMiddleware {
   constructor(
     private readonly asyncLocalStorageService: AsyncLocalStorageService,
-    private readonly logger: MyCustomLogger,
   ) {}
 
   use(req: Request, res: Response, next: NextFunction) {
