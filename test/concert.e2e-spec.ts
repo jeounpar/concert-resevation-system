bfeed0a868b7f03a183916b399354902c4f00ba4
jeounpar <cdex6531@gmail.com>
2025-01-15 17:22:19 +0900

    update: 콘서트 예약 E2E 동시성 테스트 추가

--- test/concert.e2e-spec.ts
+++ test/concert.e2e-spec.ts
@@ -12,7 +12,7 @@ import { MySqlContainer, StartedMySqlContainer } from '@testcontainers/mysql';
 import { DataSource } from 'typeorm';
 import { getAllEntities, setDataSource } from '../src/config/typeorm-factory';
 import { TokenService } from '../src/token/service/token.service';
-import { SeatEntity } from '../src/entity';
+import { ConcertScheduleEntity, SeatEntity } from '../src/entity';
 import { TokenModule } from '../src/token/token.module';
 import { ConcertModule } from '../src/concert/concert.module';
 import { PointModule } from '../src/point/point.module';
@@ -24,6 +24,7 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from '../src/interceptor/my-logging.interceptor';
 import { GenerateRequestIdMiddleware } from '../src/middleware/generate-request-id.middleware';
 import { TokenVerifyMiddleware } from '../src/middleware/token-verify.middleware';
+import { TOKEN_POLICY } from '../src/policy';
 
 describe('Concert (e2e)', () => {
   jest.setTimeout(50000);
@@ -31,6 +32,7 @@ describe('Concert (e2e)', () => {
   let tokenService: TokenService;
   let mysqlContainer: StartedMySqlContainer;
   let dataSource: DataSource;
+  let seatEntity: SeatEntity;
 
   beforeAll(async () => {
     mysqlContainer = await new MySqlContainer('mysql')
@@ -49,6 +51,7 @@ describe('Concert (e2e)', () => {
           password: mysqlContainer.getUserPassword(),
           database: mysqlContainer.getDatabase(),
           entities: getAllEntities(),
+          poolSize: 50,
           synchronize: true,
           logging: false,
         }),
@@ -103,20 +106,181 @@ describe('Concert (e2e)', () => {
       await dataSource.getRepository(entity).clear();
     }
 
+    // 콘서트 스케쥴 데이터 생성
+    const concertScheduleRepositoryOrm = dataSource.getRepository(
+      ConcertScheduleEntity,
+    );
+    await concertScheduleRepositoryOrm.save({
+      id: 1,
+      concertId: 1,
+      theDateString: '2025-01-03',
+    });
+
     // 좌석 데이터 생성
     const seatRepositoryOrm = dataSource.getRepository(SeatEntity);
-    await seatRepositoryOrm.save({
-      id: 1,
-      status: 'EMPTY',
-      price: 2000,
-      seatNumber: 1,
-      concertScheduleId: 1,
-      expireDate: null,
-    });
+
+    seatEntity = new SeatEntity();
+    seatEntity.id = 1;
+    seatEntity.status = 'EMPTY';
+    seatEntity.price = 2000;
+    seatEntity.seatNumber = 1;
+    seatEntity.concertScheduleId = 1;
+    seatEntity.expireDate = null;
+
+    await seatRepositoryOrm.save(seatEntity);
   });
 
   afterAll(async () => {
     await app.close();
     await mysqlContainer.stop();
+  });
+
+  it(`
+   *
+   * 1. 100명의 유저에게 각 5000 point 충전
+   * 2. 100명의 유저가 동시에 대기열 토큰을 발급
+   * 3. 토큰을 활성화 상태로 변경 (스케쥴러가 한번 돌아갔다고 생각)
+   * 4. 예약 가능 날짜 조회
+   * 5. 예약 가능 좌석 조회
+   * 6. 100명의 유저가 하나의 좌석에 대해 예약 요청 -> 1명만 성공하고 999명은 실패
+   * 7. 예약을 성공한 유저가 예약한 좌석에 대해 결제 요청 -> 성공
+   * 8. 결제에 성공한 유저의 토큰이 삭제되었는지 확인
+   * 9. 결제에 성공한 유저의 남은 포인트 조회
+   *`, async () => {
+    const totalUserCount = 100;
+    const userIds = Array.from({ length: totalUserCount }, (_, i) => i + 1);
+    const chargeAmount = 5000;
+
+    // 1. 100명의 유저에게 각 5000 point 충전
+    for (const userId of userIds) {
+      const response = await request(app.getHttpServer())
+        .post(`/point/${userId}/charge`)
+        .send({ amount: chargeAmount });
+
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        userId,
+        chargePoint: chargeAmount,
+        remainPoint: chargeAmount,
+      });
+    }
+
+    // 2. 100명의 유저가 동시에 대기열 토큰을 발급
+    const tokenIssueResponse = await Promise.all(
+      userIds.map(async (userId) => {
+        const response = await request(app.getHttpServer())
+          .post('/token/issue')
+          .send({ userId });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('tokenValue');
+        expect(response.body).toHaveProperty('userId', userId);
+
+        return { userId, tokenValue: response.body.tokenValue };
+      }),
+    );
+
+    // 3. 토큰을 활성화 상태로 변경 (스케쥴러가 한번 돌아갔다고 생각)
+    await tokenService.activeToken({});
+
+    const concertId = 1;
+
+    // 4. 예약 가능 날짜 조회
+    const availableTimesResponses = await Promise.all(
+      tokenIssueResponse.map(async ({ userId, tokenValue }) => {
+        const response = await request(app.getHttpServer())
+          .get(`/concert/${concertId}/available-times`)
+          .set('x-queue-token', tokenValue);
+        return { userId, status: response.status };
+      }),
+    );
+    expect(availableTimesResponses.filter((e) => e.status === 200).length).toBe(
+      TOKEN_POLICY.MAX_ACTIVE_TOKEN_COUNT,
+    );
+    expect(availableTimesResponses.filter((e) => e.status !== 200).length).toBe(
+      totalUserCount - TOKEN_POLICY.MAX_ACTIVE_TOKEN_COUNT,
+    );
+
+    // 5. 예약 가능 좌석 조회
+    const availableSeatsResponses = await Promise.all(
+      tokenIssueResponse.map(async ({ userId, tokenValue }) => {
+        const response = await request(app.getHttpServer())
+          .get(`/concert/${concertId}/available-seats?date=2025-01-03`)
+          .set('x-queue-token', tokenValue);
+        return { userId, status: response.status };
+      }),
+    );
+    expect(availableSeatsResponses.filter((e) => e.status === 200).length).toBe(
+      TOKEN_POLICY.MAX_ACTIVE_TOKEN_COUNT,
+    );
+    expect(availableSeatsResponses.filter((e) => e.status !== 200).length).toBe(
+      totalUserCount - TOKEN_POLICY.MAX_ACTIVE_TOKEN_COUNT,
+    );
+
+    // 6. 100명의 유저가 하나의 좌석에 대해 예약 요청 -> 1명만 성공하고 999명은 실패
+    const seatId = 1;
+    const reservationResults = await Promise.all(
+      tokenIssueResponse.map(async ({ userId, tokenValue }) => {
+        const response = await request(app.getHttpServer())
+          .post('/concert/reservation')
+          .set('x-queue-token', tokenValue)
+          .send({ seatId, userId });
+
+        return { userId, status: response.status, body: response.body };
+      }),
+    );
+
+    const successReservations = reservationResults.filter(
+      (res) => res.status === 201,
+    );
+    const failedReservations = reservationResults.filter(
+      (res) => res.status !== 201,
+    );
+
+    expect(successReservations.length).toBe(1);
+    expect(failedReservations.length).toBe(totalUserCount - 1);
+
+    // 7. 예약을 성공한 유저가 예약한 좌석에 대해 결제 요청 -> 성공
+    const successfulUserId = successReservations[0].userId;
+    const successfulToken = tokenIssueResponse.find(
+      (t) => t.userId === successfulUserId,
+    )?.tokenValue;
+
+    const paymentResponse = await request(app.getHttpServer())
+      .post('/payment/concert')
+      .set('x-queue-token', successfulToken)
+      .send({ seatId, userId: successfulUserId });
+
+    expect(paymentResponse.status).toBe(201);
+    expect(paymentResponse.body).toHaveProperty('concertInfo');
+    expect(paymentResponse.body.concertInfo).toEqual({
+      userId: successfulUserId,
+      seatNumber: 1,
+      status: 'PAID',
+      expiredDate: null,
+      price: seatEntity.price,
+    });
+    expect(paymentResponse.body).toHaveProperty('pointInfo');
+    expect(paymentResponse.body.pointInfo).toEqual({
+      userId: successfulUserId,
+      remainPoint: 5000 - seatEntity.price,
+    });
+
+    // 8. 결제에 성공한 유저의 토큰이 삭제되었는지 확인
+    const response = await request(app.getHttpServer())
+      .get(`/concert/${concertId}/available-times`)
+      .set('x-queue-token', successfulToken)
+      .send({ seatId });
+    expect(response.status).toBe(400);
+
+    // 9. 결제에 성공한 유저의 남은 포인트 조회
+    const pointResponse = await request(app.getHttpServer()).get(
+      `/point/${successfulUserId}`,
+    );
+    expect(pointResponse.status).toBe(200);
+    expect(pointResponse.body).toEqual({
+      userId: successfulUserId,
+      remainPoint: 5000 - seatEntity.price,
+    });
   });
 });
