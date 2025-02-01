c8cabbe10521a82760a2dbca56296c67f2372319
jeounpar <cdex6531@gmail.com>
2025-02-01 16:14:58 +0900

    update: RedisCache 클래스 추가

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,2 +1,16 @@
 export * from './my-redis.module';
 export * from './redis-spin-lock';
+export * from './redis-cache';
+
+export const CacheKeyPrefix = {
+  AVAILABLE_TIMES: 'AVAILABLE_TIMES',
+} as const;
+
+export type KeyPrefixType = keyof typeof CacheKeyPrefix;
+
+export const getCacheKey = (
+  prefix: KeyPrefixType,
+  key: string | number,
+): string => {
+  return `[${prefix}]::${key}`;
+};
--- src/redis/my-redis.module.ts
+++ src/redis/my-redis.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisSpinLock } from './redis-spin-lock';
+import { RedisCache } from './redis-cache';
 
 @Global()
 @Module({
-  providers: [RedisSpinLock],
-  exports: [RedisSpinLock],
+  providers: [RedisSpinLock, RedisCache],
+  exports: [RedisSpinLock, RedisCache],
 })
 export class MyRedisModule {}
