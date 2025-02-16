cbb916c707c6c12271eaaca977dde5f855f0e232
jeounpar <cdex6531@gmail.com>
2025-02-16 16:08:10 +0900

    update: ExternalDataPlatformEventHandler에 Outbox save 로직 추가

--- src/external/data-platform/external-data-platform-event-handler.ts
+++ src/external/data-platform/external-data-platform-event-handler.ts
@@ -1,9 +1,17 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Inject,
+} from '@nestjs/common';
 import { EventBus } from '@nestjs/cqrs';
 import { MyCustomLogger } from '../../log/my-custom-logger';
 import { filter } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { ConcertPaymentSuccessEvent } from '../../api/payment/concert-payment-success-event';
+import { ClientKafka } from '@nestjs/microservices';
+import { OutboxRepository } from '../../outbox/outbox.repository';
+import { OutboxDomain } from '../../outbox/outbox.domain';
 
 @Injectable()
 export class ExternalDataPlatformEventHandler
@@ -13,15 +21,18 @@ export class ExternalDataPlatformEventHandler
 
   constructor(
     private readonly eventBus: EventBus,
+    private readonly outboxRepository: OutboxRepository,
+    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
     private readonly logger: MyCustomLogger,
   ) {}
 
   onModuleInit() {
     this.subscription = this.eventBus
       .pipe(filter((event) => event instanceof ConcertPaymentSuccessEvent))
-      .subscribe((event: ConcertPaymentSuccessEvent) =>
-        this.handleConcertPaymentSuccess(event),
-      );
+      .subscribe((event: ConcertPaymentSuccessEvent) => {
+        this.publishConcertPaymentSuccessMessage(event);
+        this.messageOutboxHandler(event);
+      });
   }
 
   onModuleDestroy() {
@@ -30,11 +41,31 @@ export class ExternalDataPlatformEventHandler
     }
   }
 
-  private async handleConcertPaymentSuccess(event: ConcertPaymentSuccessEvent) {
-    this.logger.log(
-      'send',
-      'Sending Data to External Platform...',
-      event.payload,
-    );
+  private async messageOutboxHandler(event: ConcertPaymentSuccessEvent) {
+    this.logger.log('messageOutboxHandler', 'Handling event...', event.payload);
+    const outbox = OutboxDomain.createInitOutbox({ payload: event.payload });
+    await this.outboxRepository.save({ domain: outbox });
+  }
+
+  private async publishConcertPaymentSuccessMessage(
+    event: ConcertPaymentSuccessEvent,
+  ) {
+    try {
+      this.logger.log(
+        'handleConcertPaymentSuccess',
+        'Publish Message...',
+        event.payload,
+      );
+      this.kafkaClient.emit(
+        'CONCERT_PAYMENT_RESERVATION_SUCCESS',
+        event.payload,
+      );
+    } catch (err) {
+      this.logger.error(
+        'handleConcertPaymentSuccess',
+        'Failed publish message',
+        event.payload,
+      );
+    }
   }
 }
